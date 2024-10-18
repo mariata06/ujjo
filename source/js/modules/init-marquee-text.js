2ca1c0a5850a93599c433f9964e94cb2a1d7c939
mariata06 <slinkywizard@mail.ru>
2024-10-18 23:33:00 +0300

    Add main image, story

--- source/js/modules/init-marquee-text.js
+++ source/js/modules/init-marquee-text.js
@@ -2,20 +2,17 @@ const initMarqueeText = () => {
   document.addEventListener('DOMContentLoaded', function () {
     const marquee = document.querySelector('.marquee');
     const marqueeContent = document.querySelector('.marquee-content');
-    const contentWidth = marqueeContent.scrollWidth; // Ширина исходного содержимого
+    const contentWidth = marqueeContent.scrollWidth; // Ширина содержимого
     const totalWidth = marquee.offsetWidth; // Ширина контейнера
 
-    // Рассчитываем сколько раз нужно повторить содержимое
+    // Рассчитываем количество повторений, чтобы заполнить ширину контейнера и добавить запас для плавной анимации
     const repeatCount = Math.ceil(totalWidth / contentWidth) + 2;
 
-    // Дублируем содержимое несколько раз
+    // Дублируем содержимое несколько раз для создания бесшовного эффекта
     for (let i = 0; i < repeatCount; i++) {
       const clone = marqueeContent.cloneNode(true);
       marquee.appendChild(clone);
     }
-
-    // Устанавливаем минимальную ширину для бесшовной работы
-    marqueeContent.style.minWidth = `${contentWidth}px`;
   });
 
 };
